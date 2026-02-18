96943b067905ac0cac81b26da76ae3fecca4c072
robinvandermolen <robin@maykinmedia.nl>
2026-02-18 11:08:34 +0100

    :wrench: [#22] MSW storybook setup

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,13 +2,23 @@ import '@maykin-ui/admin-ui/style';
 // @TODO update theme after upgrading admin-ui
 import '@maykin-ui/admin-ui/style/themes/purple-rain.css';
 import type {Preview} from '@storybook/react-vite';
+import {initialize, mswLoader} from 'msw-storybook-addon';
 
 import {withAdminSettingsProvider} from '@/sb-decorators';
 
 import {reactIntl} from './reactIntl';
 
+initialize({
+  onUnhandledRequest: 'bypass',
+  serviceWorker: {
+    url: './mockServiceWorker.js',
+  },
+  quiet: true, // don't output logs
+});
+
 const preview: Preview = {
   decorators: [withAdminSettingsProvider],
+  loaders: [mswLoader],
   parameters: {
     reactIntl,
     controls: {
