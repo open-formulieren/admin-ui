d3feb50a25f6cfe84585b0dfa76f3f1156e75eaf
robinvandermolen <robin@maykinmedia.nl>
2026-03-11 16:40:07 +0100

    :sparkles: [#12] Added category type and API mock

--- src/api-mocks/index.ts
+++ src/api-mocks/index.ts
@@ -2,6 +2,7 @@ import {BASE_URL_V2, BASE_URL_V3} from './base';
 import mswWorker from './msw-worker';
 
 export * from './accounts';
+export * from './category';
 export * from './form';
 
 export {BASE_URL_V2, BASE_URL_V3, mswWorker};
