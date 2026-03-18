700b27814fb7e33e7ef3c56cd81121bd94df3a74
robinvandermolen <robin@maykinmedia.nl>
2026-03-18 16:34:08 +0100

    :sparkles: [#12] Add frontend-only data for step literals
    As all form steps in the new admin-ui share their literals, we need a single point of truth for these step literals.
    
    By adding the internal (frontend-only) value `_stepLiterals`, the form can easily manage one collection of literals. This also ensures that, even if all form steps are removed, we keep the literals stored in a safe place.
    
    Before submitting the form details to the backend, we simply replace the step literals with the literals set by `_stepLiterals` and all step literals will be synced.

--- src/queryClient/form.ts
+++ src/queryClient/form.ts
@@ -2,21 +2,63 @@ import type {QueryClient} from '@tanstack/react-query';
 import {useMutation} from '@tanstack/react-query';
 
 import {useAdminSettings} from '@/hooks/useAdminSettings';
-import type {Form} from '@/types/form';
+import type {Form, InternalForm} from '@/types/form';
 import {apiCall} from '@/utils/fetch';
 
 export const getFormDetailsQueryKey = (formId?: string) => ['formDetails', formId];
+
+/**
+ * Transforms a Form object to an InternalForm object.
+ *
+ * The InternalForm type includes properties that are used internally to make editing the
+ * form data easier.
+ */
+export const formToInternalForm = (form: Form): InternalForm => ({
+  ...form,
+  _stepLiterals: form.steps.length
+    ? form.steps[0].literals
+    : {
+        previous_text: '',
+        next_text: '',
+        save_text: '',
+      },
+});
+
+/**
+ * Transforms an InternalForm object to a Form object.
+ *
+ * The InternalForm type includes properties that should only be used internally,
+ * like _stepLiterals. When persisting a form back to the backend, these properties
+ * should not be included.
+ */
+const internalFormToForm = (internalForm: InternalForm): Form => {
+  const stepLiterals = internalForm._stepLiterals;
+
+  // The _stepLiterals data should be copied to the literals of each form step.
+  if (stepLiterals) {
+    internalForm.steps.forEach((_, index) => {
+      internalForm.steps[index].literals = stepLiterals;
+    });
+  }
+
+  // Make sure to delete the _stepLiterals property before returning the form
+  delete internalForm._stepLiterals;
+  return internalForm satisfies Form;
+};
 
 export const formLoader = (
   apiBaseUrl: string,
   queryClient: QueryClient,
   formId?: string
-): Promise<Form | undefined> => {
+): Promise<InternalForm | undefined> => {
   return queryClient.ensureQueryData({
     queryKey: getFormDetailsQueryKey(formId),
     queryFn: async () => {
       const response = await apiCall(`${apiBaseUrl}form/${formId}`);
-      if (response.ok) return response.json();
+      if (response.ok) {
+        const formData: Form = await response.json();
+        return formToInternalForm(formData);
+      }
 
       if (response.status === 404) {
         throw new Response('Form not found', {status: 404, statusText: 'Not found'});
@@ -29,11 +71,14 @@ export const formLoader = (
 
 export const useFormMutation = (queryClient: QueryClient, formId: string) => {
   const {apiBaseUrls} = useAdminSettings();
-  return useMutation<Form, Error, Form, {previous?: Form}>({
+  return useMutation<Form, Error, InternalForm, {previous?: InternalForm}>({
     mutationFn: async newFormDetails => {
+      // The backend expects a Form object, so we need to transform the internal form
+      // back to a Form object.
+      const backendForm = internalFormToForm(newFormDetails);
       const response = await apiCall(`${apiBaseUrls.v3}form/${formId}`, {
         method: 'PUT',
-        body: JSON.stringify(newFormDetails),
+        body: JSON.stringify(backendForm),
       });
 
       if (response.ok) return response.json();
@@ -42,7 +87,12 @@ export const useFormMutation = (queryClient: QueryClient, formId: string) => {
     },
 
     onSuccess: async response => {
-      queryClient.setQueryData(getFormDetailsQueryKey(formId), response);
+      // After a successful PUT update, we need to transform the form back to an
+      // internal form and update the query data in the query client.
+      queryClient.setQueryData<InternalForm>(
+        getFormDetailsQueryKey(formId),
+        formToInternalForm(response)
+      );
     },
   });
 };
